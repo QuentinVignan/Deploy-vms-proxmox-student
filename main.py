f9728872ec9eea6e8604c981339e441370f1f3c3
Quentin Vignan <quentin@MacBook-Pro-de-Quentin.local>
2024-03-22 15:56:36 +0100

    -it work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from lib.shell import list_yaml_in_folder , setup_argparse_global_var , setup_argparse_parameters
 from lib.get_project_info import display_info , get_project_info_yaml
+from lib.generate_template import create_template , create_template_file
 import argparse
 from rich.console import Console
 from time import sleep
@@ -19,18 +20,22 @@ elif result[0] == "list_config":
     info = []
     for path in list_file:
         info.append(get_project_info_yaml(path))
-    display_info(info)
+    if len(info) < 1:
+        print("No config file found indise folder -> ./config")
+    else:
+        display_info(info)
 elif result[0] == "create":
     print("je crées des vms")
 elif result[0] == "delete":
     print("je delete les vm et le project")
+elif result[0] == "interactive_config":
+    create_template_file(create_template())
 elif result[0] == "ERROR":
     print("ERROR aucun argument est ok")
 
 
-console = Console()
-tasks = [f"task {n}" for n in range(1, 11)]
-
+#console = Console()
+#tasks = [f"task {n}" for n in range(1, 11)]
 #with console.status("[bold green]Working on tasks...") as status:
 #    while tasks:
 #        task = tasks.pop(0)
