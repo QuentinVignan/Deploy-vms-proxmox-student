071c6ee6e84d0b8428cc2d4d9eab829077c01e40
Quentin Vignan <quentin.vignan@gmail.com>
2024-03-26 09:53:20 +0100

    add template script

---
+++ lib/template/template.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+VAR_ARGS=$1
+## edited by script##
+VMID=1001
+VLAN=1001
+TEMPLATE_ID=9000
+NAME=0
+DISK=0
+IP=0
+GW=0
+USER="student"
+PASSWORD=0
+VCPU=1
+MEMORY=0
+
+case $VAR_ARGS in
+    create)
+        qm clone 9000 $VMID --name $NAME --full
+        qm disk resize $VMID scsi0 $DISK
+            qm set $VMID --memory $MEMORY
+            qm set $VMID --sockets 2
+        qm set $VMID --vcpus $VCPU
+        qm set $VMID --ipconfig0 ip=$IP,gw=$GW
+        qm set $VMID --net0 virtio,bridge=vmbr0,tag=$VLAN
+        qm set $VMID --nameserver '8.8.8.8'
+        qm set $VMID --ciuser $USER
+        qm set $VMID --cipassword $PASSWORD
+        qm set $VMID --onboot 1
+        qm start $VMID
+        ;;
+    delete)
+        qm stop $VMID
+        qm destroy $VMID
+        ;;
+    reset)
+        qm stop $VMID
+        qm destroy $VMID
+        qm clone $TEMPLATE_ID $VMID --name $NAME --full
+        qm disk resize $VMID scsi0 $DISK
+            qm set $VMID --memory $MEMORY
+            qm set $VMID --sockets 2
+        qm set $VMID --vcpus $VCPU
+        qm set $VMID --ipconfig0 ip=$IP,gw=$GW
+        qm set $VMID --net0 virtio,bridge=vmbr0,tag=$VLAN
+        qm set $VMID --nameserver '8.8.8.8'
+        qm set $VMID --ciuser $USER
+        qm set $VMID --cipassword $PASSWORD
+        qm set $VMID --onboot 1
+        qm start $VMID
+        ;;
+    *)
+        echo "Usage: $0 create|delete"
+        exit 1
+esac
