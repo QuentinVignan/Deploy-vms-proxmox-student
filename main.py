542231e376ea5152970e7bf78ec80f07b5a87138
Quentin Vignan <quentin.vignan@gmail.com>
2024-03-27 11:14:27 +0100

    change main for use create vm function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from lib.shell import list_yaml_in_folder , setup_argparse_global_var , setup_argparse_parameters
 from lib.get_project_info import display_info , get_project_info_yaml
 from lib.generate_template import create_template , create_template_file
+from lib.create_delete_vm import create_vm , delete_vm
 import argparse
 
 parser = argparse.ArgumentParser(
@@ -23,19 +24,16 @@ elif result[0] == "list_config":
     else:
         display_info(info)
 elif result[0] == "create":
-    print("je crées des vms")
+    user_proxmox = input("Enter user : ")
+    password_proxmox = input("Enter password " + user_proxmox + " : ")
+    create_vm(result[1] , user_proxmox , password_proxmox)
 elif result[0] == "delete":
-    print("je delete les vm et le project")
+    user_proxmox = input("Enter user for proxmox : ")
+    password_proxmox = input("Enter password " + user_proxmox + " : ")
+    delete_vm(result[1] , user_proxmox , password_proxmox)
 elif result[0] == "interactive_config":
     create_template_file(create_template())
 elif result[0] == "ERROR":
     print("ERROR aucun argument est ok")
 
 
-#console = Console()
-#tasks = [f"task {n}" for n in range(1, 11)]
-#with console.status("[bold green]Working on tasks...") as status:
-#    while tasks:
-#        task = tasks.pop(0)
-#        sleep(5)
-#        console.log(f"{task} complete")
